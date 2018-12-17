795b88a8070356a96cb2e020e5c9a2f19f5a9269
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-12-17 16:54:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
 class Owner
-  # code goes here
+  # code goes
+  attr_accessor :species, :name, :pets
+  @@all = []
+
+  def initialize(species = "human")
+      @species = species
+      instance = self
+      @pets = {cats: [], dogs: [], fishes: []}
+      @@all << instance
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(name)
+    fishInstance = Fish.new(name)
+    @pets[:fishes] << fishInstance
+  end
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def species=(species)
+    raise NoMethodError if species
+  end
+
+  def species
+    @species = "human"
+  end
 end
